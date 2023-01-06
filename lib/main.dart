edb220fa22ddaf808d0901fd7e8591e5a8f5db7b
Roman Dzerkal <dzerkal.r@gmail.com>
2023-01-06 13:08:27 +0200

    Flutter bot added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:resources_mover/logic/mover_view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     routes: {'/mover': (context) => const MoverView()},
     initialRoute: '/mover',
+    builder: BotToastInit(), //1. call BotToastInit
+    navigatorObservers: [BotToastNavigatorObserver()],
   ));
 }

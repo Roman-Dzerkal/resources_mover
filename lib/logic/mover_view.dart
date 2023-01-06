0abd736eb2c3af454213f5700ec874bced724ffc
Roman Dzerkal <dzerkal.r@gmail.com>
2023-01-06 13:07:30 +0200

    Added some UI widgets

--- lib/logic/mover_view.dart
+++ lib/logic/mover_view.dart
@@ -8,35 +8,86 @@ class MoverView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<MoverViewModel>.reactive(
-        viewModelBuilder: () => MoverViewModel(),
-        builder: (context, model, child) => Scaffold(
-              body: Center(
-                child: Column(children: [
+      viewModelBuilder: () => MoverViewModel(),
+      builder: (context, model, child) => Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('SDK textures folder'),
+                  Container(
+                    margin: EdgeInsets.only(left: 8, right: 8),
+                    child: SizedBox(
+                      height: 50,
+                      width: 500,
+                      child: TextField(
+                        readOnly: true,
+                        controller: model.sourcePathController,
+                      ),
+                    ),
+                  ),
                   ElevatedButton(
                       onPressed: model.openSourceFolder,
                       child: const Text('Open')),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Game textures folder'),
+                  Container(
+                    margin: EdgeInsets.only(left: 8, right: 8),
+                    child: SizedBox(
+                      height: 50,
+                      width: 500,
+                      child: TextField(
+                        readOnly: true,
+                        controller: model.targetPathController,
+                      ),
+                    ),
+                  ),
                   ElevatedButton(
                       onPressed: model.openTargetFolder,
                       child: const Text('Open')),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Game .log file'),
+                  Container(
+                    margin: EdgeInsets.only(left: 8, right: 8),
+                    child: SizedBox(
+                      height: 50,
+                      width: 500,
+                      child: TextField(
+                        readOnly: true,
+                        controller: model.logFilePathController,
+                      ),
+                    ),
+                  ),
                   ElevatedButton(
                       onPressed: model.openLogFile, child: const Text('Open')),
-                  ElevatedButton(
-                      onPressed: model.startCopy, child: const Text('Start')),
-                  Row(
-                    children: [
-                      CircularProgressIndicator(
-                        value: model.progressValue,
-                        backgroundColor: Colors.black,
-                        color: Colors.white,
-                        semanticsLabel: 'Semantic lable',
-                        semanticsValue: 'Semantic value',
-                        strokeWidth: 2.5,
-                      ),
-                      Text('Copying... ${model.currentFile}')
-                    ],
-                  )
-                ]),
+                ],
               ),
-            ));
+            ],
+          ),
+          ElevatedButton(
+              onPressed: model.startCopy, child: const Text('Start move')),
+        ]),
+      ),
+    );
   }
 }
